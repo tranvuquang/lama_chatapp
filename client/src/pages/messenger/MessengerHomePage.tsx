c2c814d34382b3733263084d132d4a309133eef4
quangminh <quangminh@gmail.com>
2023-03-08 13:55:19 +0700

    br06-socket

--- client/src/pages/messenger/MessengerHomePage.tsx
+++ client/src/pages/messenger/MessengerHomePage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "../css/messenger.css";
 import { useAppDispatch, useAppSelector } from "../../app/hooks";
 import { getAxiosData, postAxiosData } from "../../axios/axiosConfig";
@@ -6,6 +6,10 @@ import { selectAuth } from "../../features/auth/authSlice";
 import Topbar from "../../features/messenger/components/Topbar/Topbar";
 import { IConversation, IMessage } from "../../features/messenger/types";
 import Message from "../../features/messenger/components/Message/Message";
+import socketIOClient from "socket.io-client";
+import { socketURL } from "../../constants";
+
+const socket = socketIOClient(socketURL);
 
 type Props = {
   conversations: IConversation[];
@@ -20,6 +24,10 @@ const MessengerHomePage = ({ conversations = [], receiverIds = [] }: Props) => {
   const [currentChat, setCurrentChat] = useState<IConversation | null>(null);
   const [newMessage, setNewMessage] = useState("");
   const [messages, setMessages] = useState<IMessage[]>([]);
+  const [arrivalMessage, setArrivalMessage] = useState<any>(null);
+
+  const scrollRef = useRef();
+  // const [onlineUsers, setOnlineUsers] = useState<string[]>([]);
 
   // lay thong tin cua cac user nhan dc tin nhan qua mang receiverIds
   useEffect(() => {
@@ -43,6 +51,60 @@ const MessengerHomePage = ({ conversations = [], receiverIds = [] }: Props) => {
       }
     })();
   }, [accessToken, dispatch, receiverIds]);
+
+  // save message vao dung dia chi cua nguoi nhan
+  useEffect(() => {
+    if (arrivalMessage) {
+      // neu tin nhan den dung voi cua so chat. chi thay doi khi co tin nhan moi den
+      if (currentChat?.members.includes(arrivalMessage.sender)) {
+        setMessages((prev) => [...prev, arrivalMessage] as any);
+      }
+      // neu chua mo cua so chat
+      if (!currentChat) {
+        console.log("!currentChat");
+      }
+      // da mo cua so chat nhung tin nhan den ko phai cua cua so chat
+      if (
+        currentChat &&
+        !currentChat?.members.includes(arrivalMessage.sender)
+      ) {
+        console.log("currentChat");
+      }
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [arrivalMessage]);
+
+  // socket logic start very importa ==========================
+  // ket noi socket va disconnect
+  useEffect(() => {
+    socket.on("connect", () => {});
+    socket.on("disconnect", () => {});
+
+    socket.on("getMessageFromSocket", (data) => {
+      setArrivalMessage({
+        sender: data.senderId,
+        text: data.text,
+        createdAt: Date.now(),
+      });
+    });
+    return () => {
+      socket.off("connect");
+      socket.off("disconnect");
+      socket.off("getMessageFromSocket");
+    };
+  }, []);
+
+  // add user khi da ket noi va get user ve client
+  useEffect(() => {
+    socket.emit("addUserFromClient", user.id);
+    socket.on("getUsersFromSocket", (users) => {
+      // setOnlineUsers(
+      //   user.followings.filter((f) =>
+      //     users.some((u: { userId: string }) => u.userId === f)
+      //   )
+      // );
+    });
+  }, [user]);
 
   // neu co cua so chat thi lay tin nhan cua cuoc hoi thoai do
   useEffect(() => {
@@ -71,6 +133,14 @@ const MessengerHomePage = ({ conversations = [], receiverIds = [] }: Props) => {
         text: newMessage,
         conversationId: currentChat?.id,
       };
+      const receiverId = currentChat?.members.find(
+        (member: string) => member !== user.id
+      );
+      socket.emit("sendMessageFromClient", {
+        senderId: user.id,
+        receiverId,
+        text: newMessage,
+      });
       const { resData, reFetchData } = (await postAxiosData(
         "/api/messages/create",
         accessToken,
@@ -85,8 +155,14 @@ const MessengerHomePage = ({ conversations = [], receiverIds = [] }: Props) => {
     }
   };
 
+  useEffect(() => {
+    const current = scrollRef.current as any;
+    current?.scrollIntoView({ behavior: "smooth" });
+  }, [messages]);
+
   return (
     <>
+      
       <Topbar />
       <div className="messenger">
         <div className="chatMenu">
@@ -122,7 +198,7 @@ const MessengerHomePage = ({ conversations = [], receiverIds = [] }: Props) => {
                     messages.length > 0 &&
                     messages.map((m, index) => {
                       return (
-                        <div key={index}>
+                        <div key={index}  ref={scrollRef as any}>
                           <Message message={m} own={m.sender === user.id} />
                         </div>
                       );
